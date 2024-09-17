28e11a0031ad9323bfe5fcfbc34852c6bcfefe93
ecolus <ecolus48@gmail.com>
2024-09-17 15:33:34 +0100

    Created the form html for adding titles.

--- gamedream/routes.py
+++ gamedream/routes.py
@@ -4,3 +4,7 @@ from gamedream import app, db
 @app.route("/")
 def home():
     return render_template("base.html")
+
+@app.route("/add_title")
+def add_title():
+    return render_template("add_title.html")
