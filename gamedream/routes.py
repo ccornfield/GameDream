fcc8bfef2235cfb318a6906de35282169441d198
ecolus <ecolus48@gmail.com>
2024-09-19 14:35:15 +0100

    Backend for add title in place

--- gamedream/routes.py
+++ gamedream/routes.py
@@ -1,10 +1,27 @@
 from flask import render_template
 from gamedream import app, db
+from gamedream.models import Wishlist, Titles
 
 @app.route("/")
 def home():
     return render_template("base.html")
 
-@app.route("/add_title")
+
+
+@app.route("/add_title", methods=["GET", "POST"])
 def add_title():
-    return render_template("add_title.html")
+    if request.method == "POST":
+        title = Titles(
+            game_title = request.form.get("game_title"),
+            publisher = request.form.get("publisher"),
+            developer = request.form.get("developer"),
+            price = request.form.get("price"),
+            genre = request.form.get("genre"),
+            platform = request.form.get("platform"),
+            description = request.form.get("description")
+        )
+        db.session.add(title)
+        db.session.commit()
+        return redirect(url_for("home"))
+        
+    return render_template("base.html")
