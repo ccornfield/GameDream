4d79997c3c6c1e44610868df620040e30c4a44d2
Charles Cornfield <ecolus48@gmail.com>
2024-10-01 19:02:48 +0000

    fix: beginning work on creating database for multiple titles per wishlist.

--- gamedream/models.py
+++ gamedream/models.py
@@ -3,13 +3,13 @@ from flask_login import UserMixin
 
 wishlist_titles = db.Table("wishlist_titles",
                            db.Column('game_wishlist_id', db.Integer, db.ForeignKey('wishlist.id')),
-                           db.Column('game_title_id', db.Integer, db.ForeignKey('titles.id')))
+                           db.Column('game_title_id', db.Integer, db.ForeignKey('titles.id', ondelete="CASCADE")nullable=True))
 
 class Wishlist(db.Model):
     # schema for the Wishlist model
     id = db.Column(db.Integer, primary_key=True)
     wishlist_name = db.Column(db.String(30), unique=True, nullable=False)
-    title_id = db.Column(db.ARRAY(db.Integer), db.ForeignKey("titles.id", ondelete="CASCADE"), nullable=True)
+    title_id = db.Column(db.Integer, db.ForeignKey("titles.id", ondelete="CASCADE"), nullable=True)
     game_title = db.Relationship('Titles', secondary=wishlist_titles, backref='game_wishlists')
     author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
     
